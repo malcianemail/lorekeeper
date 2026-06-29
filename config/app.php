511fb640de2fd8f2fb5fb3d86ddbd55fa0082336
malcian <malcianemail@gmail.com>
2026-06-29 09:41:06 +0200

    app config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Lorekeeper'),
+    'name' => env('APP_NAME', 'Malcians'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://malcians.online'),
 
     'asset_url' => env('ASSET_URL', null),
 
